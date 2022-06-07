b8a3889e238807face99e46427d76139d1f8a34d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-06-07 09:59:51 +0000

    Completed getNumberInfo function at script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Function to Display Number Information (including Invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the Input Field
+    let num = document.getElementById("mynumber").value;
+    // Check if input is valid
+    if (isNaN(num) || num.toString().length == 0 || num<2 || num>10000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 2 and 10000.`;
+    } else {
+        txt += `You have entered the number ${num}. <p>`;
+        txt += `Sum of all the amicable numbers less than ${num} is ${sumAmicableNum(num)}.`;
+    }
+
+    // Display Information in the Browser
+    document.getElementById("numinfo").innerHTML = txt;
+}
